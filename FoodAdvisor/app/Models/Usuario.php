0f1bc0091919354691d9558e22f1526655adf9c1
Jachpi <121670083+Jachpi@users.noreply.github.com>
2025-04-02 20:17:41 +0200

    Back tokenización

--- FoodAdvisor/app/Models/Usuario.php
+++ FoodAdvisor/app/Models/Usuario.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
-class Usuario extends Model
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+class Usuario extends Model implements JWTSubject
 {
-    use SoftDeletes;
+    use SoftDeletes, HasApiTokens, Notifiable;
+    
     
     protected $table = 'usuario';
     protected $primaryKey = 'ID_user';
@@ -16,5 +19,16 @@ class Usuario extends Model
     public function cestas()
     {
         return $this->hasMany(Cesta_Compra::class, 'ID_user', 'ID_user');
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+
+        return [];
     }
 }
