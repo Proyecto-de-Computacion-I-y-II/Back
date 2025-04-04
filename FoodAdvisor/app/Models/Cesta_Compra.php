02d84ddd9d5428b127e90e5b0e7bbf16cd22868a
Alara168 <alejandrolara168@gmail.com>
2025-04-04 13:39:54 +0200

    Calcular procentajes en kg o l

--- FoodAdvisor/app/Models/Cesta_Compra.php
+++ FoodAdvisor/app/Models/Cesta_Compra.php
@@ -35,19 +35,27 @@ class Cesta_Compra extends Model
 
     public function calcularPorcentajes()
     {
-        // Calcular el total de productos directamente desde la base de datos
-        $totalProductos = $this->productos()->sum('cesta_productos.cantidad');
+        // Calcular el total de productos en base a kg o l directamente desde la base de datos
+        $totalProductos = DB::table('cesta_productos')
+            ->join('producto', 'cesta_productos.ID_prod', '=', 'producto.ID_prod')
+            ->where('cesta_productos.ID_cesta', $this->ID_cesta)
+            ->whereNull('producto.deleted_at')
+            ->selectRaw('SUM(CASE WHEN producto.kg > 0 THEN cesta_productos.cantidad * producto.kg ELSE cesta_productos.cantidad * producto.l END) as total')
+            ->value('total') ?? 0;
 
         if ($totalProductos === 0) {
             return $this->actualizarPorcentajesCero();
         }
 
-        // Obtener niveles y contar productos por nivel en una sola consulta
+        // Obtener niveles y calcular la cantidad total por nivel (considerando kg o l) en una sola consulta
         $conteosPorNivel = Producto::join('nivel_piramide', 'producto.idNivel', '=', 'nivel_piramide.idNivel')
             ->join('cesta_productos', 'producto.ID_prod', '=', 'cesta_productos.ID_prod')
             ->where('cesta_productos.ID_cesta', $this->ID_cesta) // Asegurar que solo se consideran los productos de esta cesta
             ->whereNull('producto.deleted_at') // Filtra productos no borrados
-            ->select('nivel_piramide.idNivel', DB::raw('SUM(cesta_productos.cantidad) as total'))
+            ->select(
+                'nivel_piramide.idNivel',
+                DB::raw('SUM(CASE WHEN producto.kg > 0 THEN cesta_productos.cantidad * producto.kg ELSE cesta_productos.cantidad * producto.l END) as total')
+            )
             ->groupBy('nivel_piramide.idNivel')
             ->get();
 
