82370819a083c2232958f925a6364bc498361914
Jachpi <121670083+Jachpi@users.noreply.github.com>
2025-04-14 13:32:00 +0200

    Reapply "Merge branch 'Token-Laravel-Sanctum'"
    This reverts commit 0bc0ebbcffc7177f6d11f0887a7193de7dadf232.

--- FoodAdvisor/app/Models/Usuario.php
+++ FoodAdvisor/app/Models/Usuario.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Usuario extends Model
+class Usuario extends Authenticatable implements JWTSubject
 {
     use SoftDeletes;
     
@@ -16,5 +20,15 @@ class Usuario extends Model
     public function cestas()
     {
         return $this->hasMany(Cesta_Compra::class, 'ID_user', 'ID_user');
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey(); // Usually the ID
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return []; // Or add custom claims if needed
     }
 }
