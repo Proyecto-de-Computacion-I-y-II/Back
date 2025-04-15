9e0054fc510ccc7c15c71ba2904358f2516266fc
Jachpi <121670083+Jachpi@users.noreply.github.com>
2025-04-15 10:45:23 +0200

    cambio a trozos

--- FoodAdvisor/app/Http/Controllers/UsuarioController.php
+++ FoodAdvisor/app/Http/Controllers/UsuarioController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Usuario;
 use Illuminate\Support\Facades\Hash;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
 
 class UsuarioController extends Controller
 {
