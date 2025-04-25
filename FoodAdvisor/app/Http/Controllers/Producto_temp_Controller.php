a98abbcc7371a2896c5b2a13c1cc1793a3c02a56
raqgd <ragardom03@gmail.com>
2025-04-26 00:13:25 +0200

    Endpoint devuelve tambien productos saliendo

--- FoodAdvisor/app/Http/Controllers/Producto_temp_Controller.php
+++ FoodAdvisor/app/Http/Controllers/Producto_temp_Controller.php
@@ -19,38 +19,44 @@ class Producto_temp_Controller extends Controller
     
 
     // Método para obtener los productos de temporada por mes
-   public function getProductsByMonth($mes)
-{
-    $meses = [
-        1 => 'January',
-        2 => 'February',
-        3 => 'March',
-        4 => 'April',
-        5 => 'May',
-        6 => 'June',
-        7 => 'July',
-        8 => 'August',
-        9 => 'September',
-        10 => 'October',
-        11 => 'November',
-        12 => 'December'
-    ];
-
-    if (!isset($meses[$mes])) {
-        return response()->json(['error' => 'Mes no válido'], 400);
+    public function getProductsByMonth($mes)
+    {
+        $meses = [
+            1 => 'January',
+            2 => 'February',
+            3 => 'March',
+            4 => 'April',
+            5 => 'May',
+            6 => 'June',
+            7 => 'July',
+            8 => 'August',
+            9 => 'September',
+            10 => 'October',
+            11 => 'November',
+            12 => 'December'
+        ];
+    
+        if (!isset($meses[$mes])) {
+            return response()->json(['error' => 'Mes no válido'], 400);
+        }
+    
+        $nombreMes = $meses[$mes];
+    
+        // Productos con valor 1 en ese mes
+        $productos = Producto_temp::where($nombreMes, 1)
+            ->get(['idTemp', 'producto']);
+    
+        // Productos con valor 0 en ese mes
+        $productossaliendo = Producto_temp::where($nombreMes, 0)
+            ->get(['idTemp', 'producto']);
+    
+        return response()->json([
+            'mes' => ucfirst($nombreMes),
+            'productos' => $productos,
+            'productossaliendo' => $productossaliendo
+        ]);
     }
-
-    $nombreMes = $meses[$mes];
-
-    // Buscar productos donde el mes tenga un valor de 1
-    $productos = Producto_temp::where($nombreMes, 1)
-        ->get(['idTemp', 'producto']); // Seleccionamos solo las columnas necesarias
-
-    return response()->json([
-        'mes' => ucfirst($nombreMes),
-        'productos' => $productos
-    ]);
-}
+    
 
 // Método para obtener imagen, nombre e idSuper de la tabla productos
 public function getDetalles($idTemp)
